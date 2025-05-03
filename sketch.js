437fa72fcf495393b848e068daa0a1aa37eef310
ardaa83 <ardaaydin27283@gmail.com>
2025-05-03 18:45:57 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   let constraints = {
     video: {
       facingMode: { ideal: "environment" }
